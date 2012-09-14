80ad8315f41ec684d1f9d0059ce50d1eeeb4e175
Jonathan Camenisch <jonathan@camenisch.net>
2012-09-14 16:14:14 -0500

    Compile rgeo gem with specified paths to the binaries

--- lib/language_pack/ruby-rgeo.rb
+++ lib/language_pack/ruby-rgeo.rb
@@ -1,29 +1,42 @@
 class LanguagePack::Ruby < LanguagePack::Base
   def rgeo_url(filename = nil)
-    filename += '.tgz' if filename
     "https://s3.amazonaws.com/camenischcreative/heroku-binaries/rgeo/#{filename}"
   end
 
-  # alias_method :orig_default_config_vars, :default_config_vars
-  # def default_config_vars
-  #   orig_default_config_vars.tap do |vars|
-  #     vars['PATH'] << ''
-  #   end
-  # end
+  def binaries
+    {geos: '3.3', proj: '4.8'}
+  end
 
-  def install_rgeo_binary(name)
+  def binary_names
+    binaries.keys
+  end
+
+  alias_method :orig_default_config_vars, :default_config_vars
+  def default_config_vars
+    orig_default_config_vars.tap do |vars|
+      vars['BUNDLE_BUILD__RGEO'] = binary_names.map{|name| "--with-#{name}-dir=/app/bin/#{name}/lib" }.join(' ')
+    end
+  end
+
+  def install_rgeo_binary(name, version)
     bin_dir = "bin/#{name}"
     FileUtils.mkdir_p bin_dir
-    topic("Downloading #{name} from #{rgeo_url(name)}")
+    filename = "#{name}-#{version}.tgz"
+    topic("Downloading #{name} from #{rgeo_url(filename)}")
     Dir.chdir(bin_dir) do |dir|
-      run("curl #{rgeo_url(name)} -s -o - | tar xzf -")
+      run("curl #{rgeo_url(filename)} -s -o - | tar xzf -")
     end
+  end
+
+  def pwd
+    run('pwd').chomp
   end
 
   alias_method :orig_compile, :compile
   def compile
+    binaries.each do |(name, version)|
+      install_rgeo_binary(name, version)
+    end
     orig_compile
-    install_rgeo_binary('geos-3.3')
-    install_rgeo_binary('proj-4.8')
   end
 end
