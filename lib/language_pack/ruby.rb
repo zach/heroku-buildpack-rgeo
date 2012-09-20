304e63e56bbca4a90439321edf5ea2ca8690b07a
Jonathan Camenisch <jonathan@camenisch.net>
2012-09-20 15:43:41 -0500

    Back out debugging hacks from original ruby.rb language pack

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -394,7 +394,6 @@ ERROR
         # codon since it uses bundler.
         env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:$CPATH CPPATH=#{yaml_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
         puts "Running: #{bundle_command}"
-        pipe("#{env_vars} bundle config 2>&1")
         bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
 
       end
