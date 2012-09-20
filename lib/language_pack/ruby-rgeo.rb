a900fc32fb01744cf57558dc13114ded7d57b711
Jonathan Camenisch <jonathan@camenisch.net>
2012-09-20 13:54:45 -0500

    Create the directory first

--- lib/language_pack/ruby-rgeo.rb
+++ lib/language_pack/ruby-rgeo.rb
@@ -50,8 +50,10 @@ class LanguagePack::Ruby < LanguagePack::Base
     end
 
     binary_names.each {|name| puts_and_pipe "ls #{pwd}/bin/#{name}" }
+    lib_so_conf_dir = "#{pwd}/etc/ld.so.conf.d"
+    FileUtils.mkdir_p(lib_so_conf_dir)
     binary_names.each do |name|
-      File.open("#{pwd}/etc/ld.so.conf.d/#{name}.conf", 'w') do |file|
+      File.open("#{lib_so_conf_dir}/#{name}.conf", 'w') do |file|
         file.write("/app/bin/#{name}/lib")
       end
     end
